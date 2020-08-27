10af0d5763f3ee56cc70da0c625609c38e8a50a6
Dominic Mulligan <dominic.mulligan@arm.com>
2020-08-27 16:36:44 +0100

    veracruz: add wasm32-arm-veracruz target support

--- src/lib.rs
+++ src/lib.rs
@@ -42,6 +42,7 @@ pub mod int;
 
 #[cfg(any(
     all(target_arch = "wasm32", target_os = "unknown"),
+    all(target_arch = "wasm32", target_os = "veracruz"),
     all(target_arch = "arm", target_os = "none"),
     all(target_os = "optee", target_env = "trustzone"),
     all(target_vendor = "fortanix", target_env = "sgx")
